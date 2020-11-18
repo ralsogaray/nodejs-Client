099ff9d8793a69d945f816d722c49fb4c72f64e8
ralsogaray <rodrigo.alsogaray91@gmail.com>
2020-11-17 21:31:53 -0300

    Primer demo con handlebars

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const exphbs  = require('express-handlebars')
 
 const app = express()
 
 const port = 2000
 
+app.engine('handlebars', exphbs()) //le digo cual es el motor de plantillas
+app.set('view engine', 'handlebars') // le digo al servidor quien administra las plantillas. Le dice que las plantillas van a tener sintaxis handlebar
+
 app.listen(port)
+
+
+
+
 
 // MIDDLEWARE ----
 app.use(express.static('public')) //<--- PRIMERO SE BUSCA SI LA RESPUESTA SE PUEDE OBTTENER DE PUBLIC, sino vuelve a la aplicación
@@ -15,6 +23,5 @@ app.get('/:seccion', (req, res) => {
 
     const{ seccion } = req.params
 
-    res.writeHead(200,{ "Content-Type" : "text/html"})
-    res.end(`<h1>Hello from seccion <u>${seccion}</u> in HTML</h1>`)
+    res.render( 'home' )
 })
