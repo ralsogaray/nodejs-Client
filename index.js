a65f9f620e2d6ae63c39128d7c75aeba9836a2b1
ralsogaray <rodrigo.alsogaray91@gmail.com>
2020-11-17 21:12:19 -0300

    Repaso basico de contenido gestionado en el backend

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = 2000
 
 app.listen(port)
 
-app.get('/', (req, res) => {
-    res.end("Hello world from express!!")
+// MIDDLEWARE ----
+app.use(express.static('public')) //<--- PRIMERO SE BUSCA SI LA RESPUESTA SE PUEDE OBTTENER DE PUBLIC, sino vuelve a la aplicación
+// MIDDLEWARE ----
+
+
+app.get('/:seccion', (req, res) => {
+
+    const{ seccion } = req.params
+
+    res.writeHead(200,{ "Content-Type" : "text/html"})
+    res.end(`<h1>Hello from seccion <u>${seccion}</u> in HTML</h1>`)
 })
